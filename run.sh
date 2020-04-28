1b17af9aa2fc9e82877511529882e77e94e54cb6
Dragos Velicanu <dvelicanu@manifold.ai>
2020-04-28 16:08:21 -0400

    parralelize bash runner

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
+etl() {
+    python extract.py data/raw/$1 data/extracted/$2.json
+    python parse.py data/extracted/$2.json data/parsed/$2.json
+    python standardize.py data/parsed/$2.json data/standardized/$2.json
+}
+
 for i in data/raw/*
 do
     base=$(basename $i)
     filename="${base%.*}"
-    python extract.py data/raw/$base data/extracted/$filename.json
-    python parse.py data/extracted/$filename.json data/parsed/$filename.json
-    python standardize.py data/parsed/$filename.json data/standardized/$filename.json
+    etl $base $filename &
 done
+wait
 python ingest.py data/standardized/*.json expenses expenses.db
