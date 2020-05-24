057a21f647fb59d2f4fecd77f885140679a2bac5
Dragos Velicanu <dvelicanu@manifold.ai>
2020-05-24 18:59:31 -0400

    add static css and 2 buttons

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def root():
     return app.send_static_file("index.html")
 
 
+@app.route("/<path:path>")
+def static_proxy(path):
+    return app.send_static_file(path)
+
+
 if __name__ == "__main__":
     app.run()
