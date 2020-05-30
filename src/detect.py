2d33b754bfcc866cd3b7ca90cbc547eb5501d2e5
Dragos Velicanu <dvelicanu@manifold.ai>
2020-05-29 21:42:34 -0400

    add detect function

---
+++ src/detect.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+
+def _get_card_definitions(dir_):
+    print(dir_)
+    return {
+        i.replace(".json", ""): json.load(open(os.path.join(dir_, i)))
+        for i in next(os.walk(dir_))[2]
+        if i.endswith(".json")
+    }
+
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+CARD_DEFINITIONS = _get_card_definitions(os.path.join(script_dir, "card_definitions"))
+
+
+def get_card(record):
+    for card, card_def in CARD_DEFINITIONS.items():
+        if all(key in record for key in card_def):
+            return card, card_def
