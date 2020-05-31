43ce44dec2131a379da6da788a817b1b8de8dd75
Dragos Velicanu <dvelicanu@manifold.ai>
2020-05-31 08:16:16 -0400

    combine card definitions into one json"

--- src/detect.py
+++ src/detect.py
@@ -1,18 +1,9 @@
 import json
 import os
 
-
-def _get_card_definitions(dir_):
-    return {
-        i.replace(".json", ""): json.load(open(os.path.join(dir_, i)))
-        for i in next(os.walk(dir_))[2]
-        if i.endswith(".json")
-    }
-
-
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-CARD_DEFINITIONS = _get_card_definitions(os.path.join(script_dir, "card_definitions"))
+CARD_DEFINITIONS = json.load(open(os.path.join(script_dir, "card_definitions.json")))
 
 
 def get_card(record):
