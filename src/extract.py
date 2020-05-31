2e27a7b4fe9858e5f6d1f693c25319e70a0665cc
Dragos Velicanu <dvelicanu@manifold.ai>
2020-05-31 08:32:10 -0400

    read to dict --> records from file and moved to common + doocstring

--- src/extract.py
+++ src/extract.py
@@ -1,27 +1,10 @@
-import csv
-import io
 import json
 
 import click
-import numpy as np
-import pandas as pd
 
-from common import get_log
+from common import get_log, records_from_file
 
 log = get_log(__file__)
-
-
-def read_to_dict(infile):
-    payload = infile.read(2048)
-    if isinstance(infile, io.BytesIO):
-        payload = payload.decode("utf-8")
-    dialect = csv.Sniffer().sniff(payload)
-    infile.seek(0)
-    return (
-        pd.read_csv(infile, dialect=dialect)
-        .replace({np.nan: None})
-        .to_dict(orient="records")
-    )
 
 
 @click.command()
@@ -29,7 +12,7 @@ def read_to_dict(infile):
 @click.argument("outfile", type=click.File("w"))
 def extract(infile, outfile):
     """Converts excel and csv files into json"""
-    records = read_to_dict(infile)
+    records = records_from_file(infile)
     log.info(f"Extracting {infile} into {outfile.name}")
     for record in records:
         outfile.write(f"{json.dumps(record)}\n")
