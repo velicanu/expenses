5695ef0f35f5be461f0e8c6eadfebf70f7c6a94d
Dragos Velicanu <dvelicanu@manifold.ai>
2020-05-31 08:32:10 -0400

    read to dict --> records from file and moved to common + doocstring

---
+++ src/common_tests.py
@@ -0,0 +1,51 @@
+import io
+import os
+import tempfile
+import unittest
+
+from common import records_from_file
+
+input_string = """
+Transaction Date,Post Date,Description,Category,Type,Amount
+12/30/2019,12/31/2019,THE LANDING PIZZA AND KIT,Food & Drink,Sale,-44.00
+12/29/2019,12/30/2019,TST* SWEET RICE - JP,Food & Drink,Sale,-51.37
+"""
+expected = [
+    {
+        "Transaction Date": "12/30/2019",
+        "Post Date": "12/31/2019",
+        "Description": "THE LANDING PIZZA AND KIT",
+        "Category": "Food & Drink",
+        "Type": "Sale",
+        "Amount": -44.0,
+    },
+    {
+        "Transaction Date": "12/29/2019",
+        "Post Date": "12/30/2019",
+        "Description": "TST* SWEET RICE - JP",
+        "Category": "Food & Drink",
+        "Type": "Sale",
+        "Amount": -51.37,
+    },
+]
+
+
+class TestCommon(unittest.TestCase):
+    def test_records_from_file_file(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            input_ = os.path.join(tmpdir, "input.csv")
+            with open(input_, "w") as _f:
+                _f.write(input_string)
+
+            actual = records_from_file(open(input_, "r"))
+
+            self.assertEqual(expected, actual)
+
+    def test_records_from_file_stream(self):
+        with io.BytesIO() as _f:
+            _f.write(input_string.encode("utf-8"))
+            _f.seek(0)
+
+            actual = records_from_file(_f)
+
+            self.assertEqual(expected, actual)
