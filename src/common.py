94fc1c552136495633ecd7cb16829bd319459433
Dragos Velicanu <dvelicanu@manifold.ai>
2020-05-31 09:23:38 -0400

    pull file save function out of app and test it

--- src/common.py
+++ src/common.py
@@ -6,6 +6,8 @@ import os
 import numpy as np
 import pandas as pd
 
+from detect import identify_card
+
 
 def get_log(name):
     log = logging.getLogger(os.path.basename(name))
@@ -36,3 +38,25 @@ def records_from_file(infile):
         .replace({np.nan: None})
         .to_dict(orient="records")
     )
+
+
+def save_file_if_valid(file_, filename, upload_folder):
+    """
+    Saves the given file to the upload_folder if it matches a card
+
+    :param file_: a werkzeug.FileStorage object from flask
+    :param filename: the filename to save the file to
+    :param upload_folder: folder to save the file
+
+    """
+    with io.BytesIO() as stream:
+        file_.save(stream)
+        stream.seek(0)
+        first_record = records_from_file(stream)[0]
+        card, card_def = identify_card(first_record)
+        if card:
+            with open(os.path.join(upload_folder, filename), "wb") as output_file:
+                output_file.write(stream.getbuffer())
+            return "success", f"{filename}: {card}"
+        else:
+            return "failed", f"{filename}"
--- src/common_tests.py
+++ src/common_tests.py
@@ -3,10 +3,17 @@ import os
 import tempfile
 import unittest
 
-from common import records_from_file
+from werkzeug.datastructures import FileStorage
+
+from common import records_from_file, save_file_if_valid
 
 input_string = """
 Transaction Date,Post Date,Description,Category,Type,Amount
+12/30/2019,12/31/2019,THE LANDING PIZZA AND KIT,Food & Drink,Sale,-44.00
+12/29/2019,12/30/2019,TST* SWEET RICE - JP,Food & Drink,Sale,-51.37
+"""
+unknown_string = """
+Foo,Bar,Baz,Spam,Ham,Jam
 12/30/2019,12/31/2019,THE LANDING PIZZA AND KIT,Food & Drink,Sale,-44.00
 12/29/2019,12/30/2019,TST* SWEET RICE - JP,Food & Drink,Sale,-51.37
 """
@@ -49,3 +56,35 @@ class TestCommon(unittest.TestCase):
             actual = records_from_file(_f)
 
             self.assertEqual(expected, actual)
+
+    def test_save_file_if_valid_valid(self):
+        with tempfile.TemporaryDirectory() as tmpdir, tempfile.TemporaryDirectory() as upload_folder:
+            filename = "input.csv"
+            input_ = os.path.join(tmpdir, filename)
+            with open(input_, "w") as _f:
+                _f.write(input_string)
+            file_object = FileStorage(open(input_, "rb"))
+            # file_object.save(os.path.join(upload_folder, filename))
+
+            expected = "success", f"{filename}: chase"
+            actual = save_file_if_valid(file_object, filename, upload_folder)
+            self.assertEqual(expected, actual)
+
+            with open(os.path.join(upload_folder, filename), "r") as uploaded_file:
+                self.assertTrue(input_string, uploaded_file.read())
+
+    def test_save_file_if_valid_invalid(self):
+        with tempfile.TemporaryDirectory() as tmpdir, tempfile.TemporaryDirectory() as upload_folder:
+            filename = "input.csv"
+            input_ = os.path.join(tmpdir, filename)
+            with open(input_, "w") as _f:
+                _f.write(unknown_string)
+            file_object = FileStorage(open(input_, "rb"))
+            # file_object.save(os.path.join(upload_folder, filename))
+
+            expected = "failed", f"{filename}"
+            actual = save_file_if_valid(file_object, filename, upload_folder)
+            self.assertEqual(expected, actual)
+
+            with self.assertRaises(FileNotFoundError):
+                open(os.path.join(upload_folder, filename), "r")
