3c349c7fe7fceac7d2641bc2a73ca6cca40ff7e3
Dragos Velicanu <dvelicanu@manifold.ai>
2020-07-05 01:00:09 -0400

    fix parse

--- src/parse.py
+++ src/parse.py
@@ -9,9 +9,8 @@ from detect import identify_card
 log = get_log(__file__)
 
 
-def parse_record(record):
-    card, card_def = identify_card(record)
-    parsed_record = {k: record[v] for v, k in card_def.items()}
+def parse_record(record, card, card_def):
+    parsed_record = {k: record.get(v) for v, k in card_def.items()}
     if "amount" in parsed_record:
         parsed_record["amount"] = (
             float(parsed_record["amount"]) if parsed_record["amount"] else None
@@ -32,10 +31,13 @@ def parse(infile, outfile):
     """Converts extracted json files files into uniform schema"""
 
     log.info(f"Parsing {infile} into {outfile}")
+
+    card, card_def = identify_card(json.loads(open(infile).readline()))
+
     with open(infile, "r") as inf, open(outfile, "w") as outf:
         for line in inf:
             record = json.loads(line)
-            parsed_record = parse_record(record)
+            parsed_record = parse_record(record, card, card_def)
             outf.write(f"{json.dumps(parsed_record)}\n")
 
 
