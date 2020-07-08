4eac9e62c4578c3636e7239d78061220d62ee0a0
Dragos Velicanu <dragos@velicanu.com>
2020-07-08 17:31:02 +0000

    fix integration test

--- src/integration_test.py
+++ src/integration_test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 import sqlite3
@@ -25,83 +26,84 @@ class TestPipeline(unittest.TestCase):
 
             conn = sqlite3.connect(os.path.join(tmpdir, "expenses.db"))
 
-            actual = (
+            actual = sorted(
                 pd.read_sql("SELECT * FROM expenses", conn)
                 .replace({np.nan: None})
-                .to_dict(orient="records")
+                .to_dict(orient="records"),
+                key=lambda i: json.dumps(i),
             )
             expected = [
                 {
                     "date": "2018-12-18 00:00:00",
                     "description": "REI #80 BOSTON BOSTON MA",
                     "amount": -59.89,
+                    "category": "Other",
                     "source": "usbank",
                     "source_file": "usbank.csv",
-                    "category": "Other",
                 },
                 {
                     "date": "2019-01-02 00:00:00",
                     "description": "LATE FEE - PAYMENT DUE ON 01/01",
                     "amount": 27.0,
+                    "category": "Other",
                     "source": "usbank",
                     "source_file": "usbank.csv",
-                    "category": "Other",
-                },
-                {
-                    "date": "2019-12-28 00:00:00",
-                    "description": "NIGHT SHIFT BREWING @",
-                    "amount": 4.0,
-                    "source": "capital_one",
-                    "source_file": "capital_one.csv",
-                    "category": "Dining",
-                },
-                {
-                    "date": "2019-12-29 00:00:00",
-                    "description": "KOHL'S #0531",
-                    "amount": 39.98,
-                    "source": "capital_one",
-                    "source_file": "capital_one.csv",
-                    "category": "Shopping",
-                },
-                {
-                    "date": "2020-06-19 00:00:00",
-                    "description": "Trader Joe's",
-                    "amount": 10.76,
-                    "source": "amex",
-                    "source_file": "amex.csv",
-                    "category": "Groceries",
-                },
-                {
-                    "date": "2020-06-15 00:00:00",
-                    "description": "Trader Joe's",
-                    "amount": 24.17,
-                    "source": "amex",
-                    "source_file": "amex.csv",
-                    "category": "Groceries",
                 },
                 {
                     "date": "2019-06-15 00:00:00",
                     "description": "Hannaford",
                     "amount": 2.55,
-                    "source": "amex",
                     "category": "Groceries",
+                    "source": "amex",
                     "source_file": "amex.csv",
                 },
                 {
-                    "date": "2019-12-30 00:00:00",
-                    "description": "THE LANDING PIZZA AND KIT",
-                    "amount": 44.0,
-                    "source": "chase",
-                    "source_file": "chase.csv",
+                    "date": "2019-12-28 00:00:00",
+                    "description": "NIGHT SHIFT BREWING @",
+                    "amount": 4.0,
                     "category": "Dining",
+                    "source": "capital_one",
+                    "source_file": "capital_one.csv",
+                },
+                {
+                    "date": "2019-12-29 00:00:00",
+                    "description": "KOHL'S #0531",
+                    "amount": 39.98,
+                    "category": "Shopping",
+                    "source": "capital_one",
+                    "source_file": "capital_one.csv",
                 },
                 {
                     "date": "2019-12-29 00:00:00",
                     "description": "TST* SWEET RICE - JP",
                     "amount": 51.37,
+                    "category": "Dining",
                     "source": "chase",
                     "source_file": "chase.csv",
+                },
+                {
+                    "date": "2019-12-30 00:00:00",
+                    "description": "THE LANDING PIZZA AND KIT",
+                    "amount": 44.0,
                     "category": "Dining",
+                    "source": "chase",
+                    "source_file": "chase.csv",
+                },
+                {
+                    "date": "2020-06-15 00:00:00",
+                    "description": "Trader Joe's",
+                    "amount": 24.17,
+                    "category": "Groceries",
+                    "source": "amex",
+                    "source_file": "amex.csv",
+                },
+                {
+                    "date": "2020-06-19 00:00:00",
+                    "description": "Trader Joe's",
+                    "amount": 10.76,
+                    "category": "Groceries",
+                    "source": "amex",
+                    "source_file": "amex.csv",
                 },
             ]
             self.assertEqual(actual, expected)
