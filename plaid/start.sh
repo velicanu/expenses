153e97901dc5fae96b2ca0150a0ee376ea6a8940
Dragos Velicanu <dragosvelicanu@gmail.com>
2022-01-01 11:02:09 -0500

    forgot to add the env file

--- plaid/start.sh
+++ plaid/start.sh
@@ -2,6 +2,15 @@
 
 [[ -z "${PLAID_SECRET}" || -z "${PLAID_CLIENT_ID}" ]] && echo Plaid env variables PLAID_SECRET and PLAID_CLIENT_ID need to be set, exiting. && exit 1
 
+cat > quickstart/.env <<EOL
+PLAID_CLIENT_ID="${PLAID_CLIENT_ID}"
+PLAID_SECRET="${PLAID_SECRET}"
+PLAID_ENV=development
+PLAID_PRODUCTS=transactions
+PLAID_COUNTRY_CODES=US
+PLAID_REDIRECT_URI=
+EOL
+
 docker ps > /dev/null 2>&1
 [[ $? -ne 0 ]] && echo Docker not running, exiting. && exit 1
 
